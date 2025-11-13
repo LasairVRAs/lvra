f9393a06f471230ad235e4687773e0457df95e64
Lasair User <lasair@astrosurveydb1.nat.physics.ox.ac.uk>
2025-11-13 10:08:44 +0000

    public settings file location not hard coded anymore

--- lvra/kafka_consumer.py
+++ lvra/kafka_consumer.py
@@ -4,22 +4,37 @@ import yaml
 import logging
 from pathlib import Path
 from datetime import datetime
+import os
+import sys
 
 logger = logging.getLogger(__name__)
 logging.basicConfig(level=logging.INFO,
                     format="%(asctime)s [%(levelname)s] %(name)s.%(funcName)s: %(message)s")
 
-settings_path = Path(__file__).resolve().parent.parent / "data" / "public_settings.yaml"
+
+# Get the "public settings" from the environment or grab the default. 
+env_settings = os.environment.get("LVRA_SETTINGS")
+if len(sys.argv) > 1:                            # From the command line 
+    settings_path = Path(sys.argv[1])
+elif env_settings:                               # or from environment variable
+    settings_path = Path(env_settings)
+else:                                            # or go to default file
+    settings_path = Path(__file))).resolve().parent.parent / "data" / "public_settings.yaml"
+
+
 with settings_path.open("r") as settings:
     config = yaml.safe_load(settings)
-    kafka_server = config['kafka_server']
-    my_topic = config['my_topic']
-    group_id = config['group_id']
-    json_data_dir = Path(config['json_data_dir'])
+    kafka_server = config['kafka_server']        # URL of the server
+    my_topic = config['my_topic']                # topic associated with filter
+    group_id = config['group_id']                # id used to keep your "place" in queue
+    json_data_dir = Path(config['json_data_dir'])# output directory 
+
 json_data_dir.mkdir(parents=True, exist_ok=True)
 
 def main():
-    consumer = lasair_consumer(kafka_server, group_id, my_topic)
+    consumer = lasair_consumer(kafka_server, 
+                               group_id, 
+                               my_topic)
 
     # make a timestamped file for this poll/run
     ts = datetime.utcnow().strftime("%Y%m%d_%H%M%S")
