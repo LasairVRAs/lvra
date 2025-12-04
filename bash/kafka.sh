c047486b958ebe444a336d963d6f57de11064608
Lasair User <lasair@astrosurveydb1.nat.physics.ox.ac.uk>
2025-12-04 12:16:11 +0000

    export LVRA SETTINGS in kafka.sh so runs properly

--- bash/kafka.sh
+++ bash/kafka.sh
@@ -3,6 +3,7 @@ export PYTHONPATH=/home/lasair/code/lvra/
 CODEBASE=/home/lasair/code/lvra/lvra/pypeline
 PYTHON=/home/lasair/anaconda3/envs/vra/bin/python
 LOGDIR=/home/lasair/data/vra_data/logs
+export LVRA_SETTINGS=/home/lasair/code/lvra/data/public_settings.yaml
 
 $PYTHON $CODEBASE/kafka_consumer.py >> $LOGDIR/lvra_kafka_error.log 2>&1
 
