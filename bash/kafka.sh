602fa38b1872a96f37fe4133ae6c691050d94b84
Lasair User <lasair@astrosurveydb1.nat.physics.ox.ac.uk>
2025-12-04 15:17:50 +0000

    updated name of the conda environememt in bash script

--- bash/kafka.sh
+++ bash/kafka.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/bash
 export PYTHONPATH=/home/lasair/code/lvra/
 CODEBASE=/home/lasair/code/lvra/lvra/pypeline
-PYTHON=/home/lasair/anaconda3/envs/vra/bin/python
+PYTHON=/home/lasair/anaconda3/envs/lvra/bin/python
 LOGDIR=/home/lasair/data/vra_data/logs
 export LVRA_SETTINGS=/home/lasair/code/lvra/data/public_settings.yaml
 
