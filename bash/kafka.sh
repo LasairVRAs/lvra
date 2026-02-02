61c4bd9d9177c414ac2c56f5b5797c48cbabaf0d
Lasair User <lasair@astrosurveydb1.nat.physics.ox.ac.uk>
2026-02-02 17:25:38 +0000

    fixed my kafka bash script

--- bash/kafka.sh
+++ bash/kafka.sh
@@ -2,12 +2,20 @@
 export PYTHONPATH=/home/lasair/code/lvra/
 CODEBASE=/home/lasair/code/lvra/lvra/pypeline
 PYTHON=/home/lasair/anaconda3/envs/lvra/bin/python
-LOGDIR=/home/lasair/data/vra_data/logs
+LOGBASE=/home/lasair/data/lvra/logs
 export LVRA_SETTINGS=/home/lasair/code/lvra/data/public_settings.yaml
 
 
 DATE=$(date +"%Y%m%d")
 YEAR=$(date +"%Y")
 
-$PYTHON $CODEBASE/kafka_consumer.py >> $LOGDIR/$YEAR/$DATE/lvra_kafka_error.log 2>&1 
+LOGDIR="$LOGBASE/$YEAR/$DATE"
+
+if [ ! -d "$LOGDIR" ]; then
+  echo "$LOGDIR does not exist. Creating."
+  mkdir -p $LOGDIR
+fi
+
+
+$PYTHON $CODEBASE/kafka_consumer.py >> $LOGDIR/lvra_kafka_error.log 2>&1 
 
