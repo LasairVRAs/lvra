043cd7b33f3daa8c43f88e0d8950f140c2908ac2
Heloise Stevance <hfstevance@gmail.com>
2026-02-03 14:43:01 +0000

    hot-fix: case if the alert field not populated

--- test/test_utilsfeatures.py
+++ test/test_utilsfeatures.py
@@ -6,9 +6,9 @@ test_csv_path = Path(__file__).resolve().parent.parent / "data" / "test" /"20260
 
 
 def test_json2cleandf():
-    clean_df = json2cleandf(test_json_path)
+    clean_df, objectIds_withoutAlert_col = json2cleandf(test_json_path)
     # NOTE: This is test file specific
     assert 'diaObjectId' in clean_df.columns, "diaObjectId column should exist in clean data frame"
     assert 'diaSourceId' in clean_df.columns, "diaSourceId column should exist in clean data frame"
-    assert clean_df.shape[0] == 44, "The clean data frame should have 44 rows. WARNING: TEST FILE SPECIFIC"
+    assert clean_df.shape[0] == 43, "The clean data frame should have 44 rows. WARNING: TEST FILE SPECIFIC"
     assert clean_df.shape[1] == 120, "The clean data frame should have 119 columns. WARNING: TEST FILE SPECIFIC"
